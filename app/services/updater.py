01368b60ff69098aea5c93ad2b09e5699d64b716
Alperen <alperen@example.com>
2026-09-04 00:24:36 +0300

    chore: set default updater repo to kasimalperenyavuz-design/koala-cut

--- app/services/updater.py
+++ app/services/updater.py
@@ -24,7 +24,7 @@ from app.services.storage import get_storage_dirs
 logger = logging.getLogger(__name__)
 
 CURRENT_VERSION = "1.0.0"
-DEFAULT_GITHUB_REPO = "alperen/koala-cut"
+DEFAULT_GITHUB_REPO = "kasimalperenyavuz-design/koala-cut"
 
 
 def parse_version(ver_str: str) -> tuple[int, ...]:
